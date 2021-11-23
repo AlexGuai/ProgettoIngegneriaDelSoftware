7e436daaa7b615f9643e8822a17a427c3e1f5209
Maris945 <75133028+Maris945@users.noreply.github.com>
2021-11-23 14:57:12 +0100

    Aggiustare VistaOggetto.py

--- oggetto/view/VistaOggetto.py
+++ oggetto/view/VistaOggetto.py
@@ -2,57 +2,9 @@ from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
 
 
 class VistaOggetto(QWidget):
-    def __init__(self, cliente, rimuovi_cliente, elimina_callback, parent=None):
-        super(VistaCliente, self).__init__()
+    def __init__(self, oggetto, rimuovi_cliente, elimina_callback, parent=None):
+        super(VistaOggetto, self).__init__()
         self.controller = ControlloreCliente(cliente)
         self.rimuovi_cliente = rimuovi_cliente
         self.elimina_callback = elimina_callback
 
-        h_layout = QHBoxLayout()
-        v_layout = QVBoxLayout()
-
-        label_nome = QLabel(self.controller.get_nome_cliente())
-        font_nome = label_nome.font()
-        font_nome.setPointSize(30)
-        label_nome.setFont(font_nome)
-        v_layout.addWidget(label_nome)
-
-        v_layout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
-
-        v_layout.addWidget(self.get_label_info("nome", self.controller.get_nome_cliente()))
-        v_layout.addWidget(self.get_label_info("cognome", self.controller.get_cognome_cliente()))
-        v_layout.addWidget(self.get_label_info("datadinascita", self.controller.get_datadinascita_cliente()))
-        v_layout.addWidget(self.get_label_info("email", self.controller.get_email_cliente()))
-        v_layout.addWidget(self.get_label_info("telefono", self.controller.get_telfono_cliente()))
-
-        v_layout.addItem(QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding))
-
-        btn_abbonamento = QPushButton("Abbonamento")
-        btn_abbonamento.clicked.connect(self.check_abbonamento)
-        v_layout.addWidget(btn_abbonamento)
-
-        v_layout.addItem(QSpacerItem(20, 20, QSizePolicy.Minimum, QSizePolicy.Expanding))
-
-        btn_elimina = QPushButton("Elimina")
-        btn_elimina.clicked.connect(self.elimina_cliente_click)
-        v_layout.addWidget(btn_elimina)
-
-        self.setLayout(v_layout)
-        self.setWindowTitle(self.controller.get_nome_cliente() + " " + self.controller.get_cognome_cliente())
-
-    def get_label_info(self, testo, valore):
-        current_label = QLabel("{}: {}".format(testo, valore))
-        current_font = current_label.font()
-        current_font.setPointSize(17)
-        current_label.setFont(current_font)
-        return current_label
-
-    def check_abbonamento(self):
-        self.vista_abbonamento = VistaAbbonamento(self.controller.get_abbonamento_cliente(),
-                                                  self.controller.aggiungi_nuovo_abbonamento_cliente)
-        self.vista_abbonamento.show()
-
-    def elimina_cliente_click(self):
-        self.rimuovi_cliente(self.controller.get_nome_cliente())
-        self.elimina_callback()
-        self.close()
