89502c590a7efef0a75b89992e9a1f765587323e
AlexGuai <61745907+AlexGuai@users.noreply.github.com>
2022-12-04 14:57:26 +0100

    Revert "27.11 12:22"
    This reverts commit 47f8ec25ce35174cba13a0718a31723beda30c28.

--- home/views/backup.py
+++ home/views/backup.py
@@ -1,79 +1,35 @@
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy, QLabel
+from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QSizePolicy
+
 from listaclienti.views.VistaListaClienti import VistaListaClienti
 from listadipendenti.views.VistaListaDipendenti import VistaListaDipendenti
 from listaoggettipersi.views.VistaListaOggettiPersi import VistaListaOggettiPersi
 from listaprenotazioni.views.VistaListaPrenotazioni import VistaListaPrenotazioni
 from listaservizi.views.VistaListaServizi import VistaListaServizi
-from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Home(object):
-    def setupUi(self, Home):
-        Home.setObjectName("Home")
-        Home.setEnabled(True)
-        Home.resize(868, 628)
-        Home.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
-        Home.setStyleSheet("")
+class VistaHome(QWidget):
 
-        self.centralwidget = QtWidgets.QWidget(Home)
-        self.centralwidget.setObjectName("centralwidget")
+    def __init__(self, parent=None):
+        super(VistaHome, self).__init__(parent)
+        grid_layout = QGridLayout()
 
-        self.benvenuto = QtWidgets.QLabel(self.centralwidget)
-        self.benvenuto.setGeometry(QtCore.QRect(240, 160, 300, 41))
-        self.benvenuto.setStyleSheet("font: 75 italic 12pt \"MS Sans Serif\";")
-        self.benvenuto.setFrameShape(QtWidgets.QFrame.NoFrame)
-        self.benvenuto.setTextFormat(QtCore.Qt.PlainText)
-        self.benvenuto.setAlignment(QtCore.Qt.AlignCenter)
-        self.benvenuto.setObjectName("benvenuto")
+        grid_layout.addWidget(self.get_generic_button("Lista Servizi", self.go_lista_servizi), 0, 0)
+        grid_layout.addWidget(self.get_generic_button("Lista Clienti", self.go_lista_clienti), 0, 1)
+        grid_layout.addWidget(self.get_generic_button("Lista Dipendenti", self.go_lista_dipendenti), 1, 0)
+        grid_layout.addWidget(self.get_generic_button("Lista Prenotazioni", self.go_lista_prenotazioni), 1, 1)
+        grid_layout.addWidget(self.get_generic_button("Lista Oggetti Smarriti", self.go_lista_oggetti_smarriti), 2, 0)
 
-        self.sfondo = QtWidgets.QLabel(self.centralwidget)
-        self.sfondo.setGeometry(QtCore.QRect(0, 0, 868, 628))
-        self.sfondo.setText("")
-        self.sfondo.setPixmap(QtGui.QPixmap("GUI/immaginiprogetto/MicrosoftTeams-image.png"))
-        self.sfondo.setScaledContents(True)
-        self.sfondo.setAlignment(QtCore.Qt.AlignCenter)
-        self.sfondo.setObjectName("sfondo")
-
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(70, 210, 181, 31))
-        self.pushButton.setStyleSheet("font: 75 italic 12pt \"MS Sans Serif\";")
-        self.pushButton.setObjectName("pushButton")
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(70, 250, 181, 31))
-        self.pushButton_2.setStyleSheet("font: 75 italic 12pt \"MS Sans Serif\";")
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_3.setGeometry(QtCore.QRect(70, 290, 181, 31))
-        self.pushButton_3.setStyleSheet("font: 75 italic 12pt \"MS Sans Serif\";")
-        self.pushButton_3.setObjectName("pushButton_3")
-        self.pushButton_4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_4.setGeometry(QtCore.QRect(70, 330, 181, 31))
-        self.pushButton_4.setStyleSheet("font: 75 italic 12pt \"MS Sans Serif\";")
-        self.pushButton_4.setObjectName("pushButton_4")
-        self.pushButton_5 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_5.setGeometry(QtCore.QRect(70, 370, 181, 31))
-        self.pushButton_5.setStyleSheet("font: 75 italic 12pt \"MS Sans Serif\";")
-        self.pushButton_5.setObjectName("pushButton_5")
-        Home.setCentralWidget(self.centralwidget)
-
-        self.retranslateUi(Home)
-        self.pushButton.clicked.connect(self.go_lista_clienti)
-        self.pushButton_2.clicked.connect(self.go_lista_prenotazioni)
-        self.pushButton_3.clicked.connect(self.go_lista_servizi)
-        self.pushButton_4.clicked.connect(self.go_lista_dipendenti)
-        self.pushButton_5.clicked.connect(self.go_lista_oggetti_smarriti)
-        QtCore.QMetaObject.connectSlotsByName(Home)
-
-    def retranslateUi(self, Home):
-        _translate = QtCore.QCoreApplication.translate
-        Home.setWindowTitle(_translate("Home", "MainWindow"))
-        self.pushButton.setText(_translate("Home", "CLIENTI"))
-        self.pushButton_2.setText(_translate("Home", "PRENOTAZIONI"))
-        self.pushButton_3.setText(_translate("Home", "SERVIZI"))
-        self.pushButton_4.setText(_translate("Home", "DIPENDENTI"))
-        self.pushButton_5.setText(_translate("Home", "OGGETTI"))
-
+        self.setLayout(grid_layout)
+        self.resize(400, 300)
+        self.setWindowTitle("Gestore Piscina Comunale")
+    '''
+    questa funzione restituisce un bottone generico dato il titolo
+    '''
+    def get_generic_button(self, titolo, on_click=None):
+        button = QPushButton(titolo)
+        button.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        button.clicked.connect(on_click)
+        return button
 
     def go_lista_servizi(self):
         self.vista_lista_servizi = VistaListaServizi()
@@ -93,4 +49,3 @@ class Ui_Home(object):
 
     def go_lista_oggetti_smarriti(self):
         self.vista_oggetti_persi = VistaListaOggettiPersi()
-        self.vista_oggetti_persi.show()
